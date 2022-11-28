c3b02021ec49328014439f1bbe1d39001e12d478
nik_nik <nikbox@gmail.com>
2022-11-28 16:30:52 +0300

    + deleteMovie

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,16 @@
       <h2>My Favorite Movies</h2>
     </header>
     <div class="tabs">
-      <button :class="['btn', { btn_green: movieStore.activeTab === 1 }]">
+      <button
+        :class="['btn', { btn_green: movieStore.activeTab === 1 }]"
+        @click="setTab(1)"
+      >
         Favorite
       </button>
-      <button :class="['btn', { btn_green: movieStore.activeTab === 2 }]">
+      <button
+        :class="['btn', { btn_green: movieStore.activeTab === 2 }]"
+        @click="setTab(2)"
+      >
         Search
       </button>
     </div>
@@ -37,6 +43,10 @@ import { useMovieStore } from "./stores/MovieStore";
 import Movie from "./components/Movie.vue";
 
 const movieStore = useMovieStore();
+
+const setTab = (id) => {
+  movieStore.setActiveTab(id);
+};
 </script>
 
 <style scoped>
